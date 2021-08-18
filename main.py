94fdcd462cc4625141dcd0487cb542af0236f38c
femperox <ifilchukova1@gmail.com>
2021-08-18 20:30:18 +0300

    Доработка логики взаимодействия vk-шных функций и googl'вских

--- main.py
+++ main.py
@@ -24,7 +24,22 @@ def createNamedRange(who, num:int, what = "Collect"):
 
     return result
 
-def createMessage(collect, participantList, lotWallUrl, dorazbivWallUrl = None, where = "Краснодар" ):
+def createDorazbivStrings(lotWallUrl):
+    '''
+    Создаёт строки со ссылками на доразбивы
+
+    :param lotWallUrl:
+    :return:
+    '''
+
+    dorazbivStrings = ""
+    for i in range(1, len(lotWallUrl)):
+        dorazbivStrings += "Доразбив{0}: {1}\n".format("" if i==1 else i, lotWallUrl[i])
+
+    return dorazbivStrings
+
+
+def createMessage(collect, participantList, lotWallUrl, where = "Краснодар" ):
     '''
     Создаёт текст комента в обсуждении по заданным параметрам
 
@@ -36,13 +51,11 @@ def createMessage(collect, participantList, lotWallUrl, dorazbivWallUrl = None, 
     :return:
     '''
 
-    dorazbiv = "" if dorazbivWallUrl == None else "Доразбив: {0}\n".format(dorazbivWallUrl)
-
     collectType, collectNum = table.sp.defineCollectType(collect)
 
     mes = collectType +" "+str(collectNum) +"\n"\
-          "Лот: {0}\n".format(lotWallUrl) +\
-          dorazbiv +\
+          "Лот: {0}\n".format(lotWallUrl[0]) +\
+          createDorazbivStrings(lotWallUrl) +\
           "\nСостояние: Выкупается \n\n" +\
           participantList +\
           "\nПоедет в {0}".format(where) # сделать определение куда
@@ -52,12 +65,17 @@ def createMessage(collect, participantList, lotWallUrl, dorazbivWallUrl = None, 
 
 def strToList(string):
     '''
-    Переводит строку в список
+    Переводит строку в отсортированный список
 
     :param string:
     :return:
     '''
-    return string.split(", ")
+
+    list = string.split(", ")
+    list = [int(x) for x in list]
+    list.sort()
+
+    return list
 
 def checkItems(itemsList, itemsNumList):
     '''
@@ -116,15 +134,23 @@ def checkParticipants(participantsList, items_num):
         tryMakeCorrectItemList(inp, items_num, participantsList[i], correctList)
 
     if len(items_num) != 0:
+        pprint(items_num)
         inp = input("There are {} items, which were not used! Add participants? y/n".format(len(items_num)))
         if inp == "y":
             inp = int(input("How many: "))
 
             for i in range(inp):
-                name, id = input("Enter name and vk id of participant:' ").split(" ")
+                name, id = input("Enter name and vk id of participant. if it's you id = 0:' ").split(" ")
                 items = input("Items: ")
+                if id == "0":
+                    tryMakeCorrectItemList(items, items_num, name, correctList)
+                else:
+                    tryMakeCorrectItemList(items, items_num, (id, name), correctList)
 
-                tryMakeCorrectItemList(items, items_num, (name,id), correctList)
+    if len(items_num) != 0:
+        pprint(items_num)
+        for x in items_num:
+            correctList.append(([x], " "))
 
     return correctList
 
@@ -139,12 +165,15 @@ def transformToTableFormat(participantsList):
     pList = []
 
     for p in participantsList:
-        hyperlink = '=HYPERLINK("{0}"; "{1}")'.format(p[1][0],p[1][1])
+        if isinstance(p[1], str):
+            hyperlink = p[1]
+        else:
+            hyperlink = '=HYPERLINK("{0}"; "{1}")'.format(p[1][0],p[1][1])
         pList.append((p[0],hyperlink))
 
     pList = \
         { "participants" : len(pList),
-          "participantList": pList
+          "participantList": pList,
         }
 
     return pList
@@ -171,16 +200,40 @@ def transformToTopicFormat(participantsList):
 
     for p in participantsList:
         items = table.sp.listToString(p[0])
-        participant = "@{0}({1})".format(getIdFromUrl(p[1][0]), p[1][1])
+        if isinstance(p[1], str):
+            participant = p[1]
+        else:
+            participant = "@{0}({1})".format(getIdFromUrl(p[1][0]), p[1][1])
         pList += "{0} - {1}\n".format(items, participant)
 
     return pList
 
-def createTableTopic(post_url, spId=0, topicName=0, items=0, img_url="https://i.pinimg.com/originals/50/d8/03/50d803bda6ba43aaa776d0e243f03e7b.png"):
+def makeDistinctList(post_url):
+    '''
+    Создаёт список уникальных пользователей с нескольких/одной записи
+
+    :param post_url: список адресов записей на стене сообщества
+    :return:
+    '''
+
+    participantsList = []
+    for url in post_url:
+        participantsList.append(vk.get_active_comments_users_list(url))
+
+    if len(participantsList)> 1:
+        for i in range(len(participantsList) - 1):
+            pl = list(set(participantsList[i][0] + participantsList[i + 1][0]))
+    else:
+        pl = participantsList[0][0]
+
+    return pl
+
+
+def createTableTopic(post_url, collectNum = 0, spId=0, topicName=0, items=0, img_url="https://i.pinimg.com/originals/50/d8/03/50d803bda6ba43aaa776d0e243f03e7b.png"):
     '''
     Создаёт таблицу и комент в обсуждении по заданным параметрам
 
-    :param post_url:
+    :param post_url: список адресов записей на стене сообщества
     :param spId:
     :param topicName:
     :param items:
@@ -188,18 +241,22 @@ def createTableTopic(post_url, spId=0, topicName=0, items=0, img_url="https://i.
     :return:
     '''
 
-    namedRange = createNamedRange("D", 1)
+    namedRange = createNamedRange("L", collectNum)
 
     table.createTable(spId, namedRange, participants= items, image = img_url)
 
-    participantsList = vk.get_active_comments_users_list(post_url)
+    participantsList = makeDistinctList(post_url)
+    participantsList = checkParticipants(participantsList, items)
+    participantsList.sort()
 
-    participantsList = checkParticipants(participantsList[0], items)
+    mes = createMessage(namedRange, transformToTopicFormat(participantsList), post_url, where= "Железнодорожный")
 
-    table.updateTable(namedRange, transformToTableFormat(participantsList))
 
-    mes = createMessage(namedRange, transformToTopicFormat(participantsList),post_url )
-    vk.post_comment(topicName, mes, img_urls=[img_url])
+    topicUrl = vk.post_comment(topicName, mes, img_urls=[img_url])
+
+    table.updateTable(namedRange, transformToTableFormat(participantsList), topicUrl)
+
+
 
 
 if __name__ == '__main__':
@@ -207,9 +264,15 @@ if __name__ == '__main__':
     vk = vki.BoardBot()
     table = gt.GoogleTabs()
 
-    createTableTopic("https://vk.com/wall-200887174_7949", topicName= "Лоты и индивидуалки", spId = 158683993, items= 5)
-
-
     pprint(table.sp.spreadsheetsIds)
+
+    topicName = "Лоты и индивидуалки"
+
+    wallPosts = ['https://vk.com/wall-200887174_8069', 'https://vk.com/wall-200887174_8098']
+
+    createTableTopic(wallPosts,collectNum=159, topicName= topicName, spId = 0, items= 19, img_url= 'https://sun9-41.userapi.com/impg/YndPJ7b_fS4ow_mH6hczCi1CDm0ThuQ5nPQR6w/_IqQa9M2eEE.jpg?size=1200x900&quality=96&sign=cba1d5a273b4af02298b107a36418406&type=album')
+
+
+
 
 

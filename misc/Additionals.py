9e1167d6db27fd63e277d7a9cd5610524d666f05
femperox <ifilchukova1@gmail.com>
2022-02-11 14:23:16 +0300

    Вспомогательные функции

---
+++ misc/Additionals.py
@@ -0,0 +1,14 @@
+def concatList(list1, list2):
+    '''
+    связывает два массива типа [ [...], ... [...]] поэлементно
+
+    :param list1:
+    :param list2:
+    :return: список
+    '''
+
+    resultList = []
+    for i in range(len(list1)):
+        resultList.append([list1[i], list2[i]])
+
+    return resultList
